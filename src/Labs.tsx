0515a207abe83c723d31b35ac02ff535eb2c2c3c
AkshayTolani <107060419+AkshayTolani@users.noreply.github.com>
2024-09-10 20:58:10 -0400

    first commit

---
+++ src/Labs.tsx
@@ -0,0 +1,16 @@
+export default function Labs() {
+    return (
+    <div>
+    <h2> Lab 1</h2>
+      <h3> Excercise 1.1</h3>
+      <h3> Excercise 1.2</h3>
+      <h2> Lab 2</h2>
+      <h3> Excercise 2.1</h3>
+      <h3> Excercise 2.2</h3>
+      <h2> Lab 3</h2>
+      <h3> Excercise 3.1</h3>
+      <h3> Excercise 3.2</h3>
+      <h2> Lab 4</h2>
+    </div>
+    )
+}

c9fd0f4d946bbfc05786bde197cdfd3fb8e506c4
AkshayTolani <107060419+AkshayTolani@users.noreply.github.com>
2024-09-23 11:45:19 -0400

    uptoCourses

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Kanbas from "./Kanbas"
 import Labs from "./Labs"
 import logo from './logo.svg';
 import './App.css';
 function App() {
   return (
-    <div>
-      <Labs />
-    </div>
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Kanbas" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 

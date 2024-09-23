d8da9acb6b072e89adf1528413c1644f4e856317
AkshayTolani <107060419+AkshayTolani@users.noreply.github.com>
2024-09-23 11:13:12 -0400

    UptoReactRouter

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -3,7 +3,7 @@ import Lab1 from "./Lab1"
 export default function Labs() {
     return (
         <div id = "wd-labs">
-            <h1> Labs</h1>
+            <h1>Labs</h1>
             <Lab1 />
         </div>
     );

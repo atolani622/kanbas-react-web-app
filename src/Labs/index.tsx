c193bf2d3fd1da9bf22deccc351b963142edc49f
AkshayTolani <107060419+AkshayTolani@users.noreply.github.com>
2024-09-24 11:45:30 -0400

    added_final_steps

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,7 +7,7 @@ import Lab3 from "./Lab3";
 export default function Labs() {
     return (
       <div>
-        <h1>Labs</h1>
+        <h1>Labs by Akshay Tolani</h1>
         <TOC />
         <Routes>
           <Route path="/" element={<Navigate to="Lab1" />} />

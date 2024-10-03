912b157be732c45c8e9ddd40dafe1a38af66a6b1
AkshayTolani <107060419+AkshayTolani@users.noreply.github.com>
2024-10-03 15:39:31 -0400

    finish_bootstrap_labs

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -12,9 +12,15 @@ import Float from "./Float";
 import GridLayout from "./GridLayout";
 import Flex from "./Flex";
 import ReactIconsSampler from "./ReactIcons";
+import BootstrapGrids from "./BootstrapGrids";
+import ScreenSizeLabel from "./ScreenSizeLabel";
+import BootstrapTables from "./BootstrapTables";
+import BootstrapLists from "./BootstrapLists";
+import BootstrapForms from "./BootstrapForms";
 
 export default function Lab2() {
   return (
+    <div className = "container" > 
     <div id="wd-lab2">
       <h2>Lab 2 - Cascading Style Sheets</h2>
       <h3>Styling with the STYLE attribute</h3>
@@ -72,6 +78,12 @@ export default function Lab2() {
       <GridLayout />
       <Flex />
       <ReactIconsSampler />
+      <BootstrapGrids />
+      <ScreenSizeLabel />
+      <BootstrapTables />
+      <BootstrapLists />
+      <BootstrapForms />
+      </div>
     </div>
   );
 }

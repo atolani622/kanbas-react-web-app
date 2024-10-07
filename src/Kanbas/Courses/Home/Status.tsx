d1983de6917f27000fa401987a4312b3a15e5203
AkshayTolani <107060419+AkshayTolani@users.noreply.github.com>
2024-10-07 16:15:49 -0400

    home_buttons

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -1,35 +1,36 @@
-export default function CourseStatus() {
-  return (
-    <div id="wd-course-status">
-      <h2>Course Status</h2>
-      <table>
-        <tr>
-          <td><button>Unpublish</button></td>
-          <td><button>Publish</button></td>
-        </tr>
-        <tr>
-          <td><button>Import Existing Content</button></td>
-        </tr>
-        <tr>
-          <td><button>Import from Commons</button></td>
-        </tr>
-        <tr>
-          <td><button>Choose Home Page</button></td>
-        </tr>
-        <tr>
-          <td><button>View Course Stream</button></td>
-        </tr>
-        <tr>
-          <td><button>New Announcement</button></td>
-        </tr>
-        <tr>
-          <td><button>New Analytics</button></td>
-        </tr>
-        <tr>
-          <td><button>View Course Notifications</button></td>
-        </tr>
-      </table>
-    </div>
+import { MdDoNotDisturbAlt } from "react-icons/md";
+import { FaCheckCircle, FaBullhorn } from "react-icons/fa";
+import { BiImport } from "react-icons/bi";
+import { LiaFileImportSolid } from "react-icons/lia";
+import { BsHouseDoorFill, BsBarChartFill, BsBellFill} from "react-icons/bs";
 
-  );
-}
+export default function CourseStatus () {
+  return (
+    <div id="wd-course-status" style={{ width: "300px" }}>
+      <h2>Course Status</h2>
+      <div className="d-flex">
+        <div className="w-50 pe-1">
+          <button className="btn btn-lg btn-secondary w-100 text-nowrap ">
+            <MdDoNotDisturbAlt className="me-2 fs-5" /> Unpublish </button>
+        </div>
+        <div className="w-50">
+          <button className="btn btn-lg btn-success w-100">
+            <FaCheckCircle className="me-2 fs-5" /> Publish </button>
+        </div>
+      </div><br />
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <BiImport className="me-2 fs-5" /> Import Existing Content </button>
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <LiaFileImportSolid className="me-2 fs-5" /> Import from Commons </button>
+      <button className = "btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <BsHouseDoorFill className = "me-2 fs-5" /> Choose Home Page  </button>
+      <button className = "btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <BsBarChartFill className = "me-2 fs-5" /> View Course Screen  </button>
+      <button className = "btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <BsBarChartFill className = "me-2 fs-5" /> New Annoucement  </button>
+      <button className = "btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <FaBullhorn className = "me-2 fs-5" /> New Analytics  </button>
+      <button className = "btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <BsBellFill className = "me-2 fs-5" /> View Course Notifications  </button>
+    </div>
+);}
--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,4 +1,7 @@
+import LessonControlButtons from "./LessonControlButtons";
+import ModuleControlButtons from "./ModuleControlButtons";
 import ModulesControls from "./ModulesControls";
+import { BsGripVertical } from 'react-icons/bs';
 
 export default function Modules() {
   return (
@@ -6,27 +9,47 @@ export default function Modules() {
       <ModulesControls /> <br /> <br /> <br /> <br />
       <ul id="wd-modules" className="list-group rounded-0">
         <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary">Week 1</div>
+          <div className="wd-title p-3 ps-2 bg-secondary">
+          <BsGripVertical className="me-2 fs-3" /> Week 1
+          <ModuleControlButtons />
+          </div>
           <ul className="wd-lessons list-group rounded-0">
             <li className="wd-lesson list-group-item p-3 ps-1">
+            <BsGripVertical className="me-2 fs-3" />
               LEARNING OBJECTIVES
+              <LessonControlButtons /> </li>
               <li className="wd-lesson list-group-item p-3 ps-1">
-                Introduction to the course</li>
+              <BsGripVertical className="me-2 fs-3" />
+                Introduction to the course
+                <LessonControlButtons /> </li>
               <li className="wd-lesson list-group-item p-3 ps-1">
-                Learn what is Web Development</li>
-              <li className="wd-lesson list-group-item p-3 ps-1"> Lesson 1</li>
-              <li className="wd-lesson list-group-item p-3 ps-1"> Lesson 2</li>
-              </li>
+                <BsGripVertical className="me-2 fs-3" />
+                Learn what is Web Development
+                <LessonControlButtons /> </li>
+              <li className="wd-lesson list-group-item p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" /> Lesson 1
+              <LessonControlButtons /></li>
+              <li className="wd-lesson list-group-item p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" /> Lesson 2
+              <LessonControlButtons /></li>
           </ul>
         </li>
         <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary"> Week 2 </div>
+          <div className="wd-title p-3 ps-2 bg-secondary"> 
+          <BsGripVertical className="me-2 fs-3" /> Week 2 
+          <ModuleControlButtons /> </div>
           <ul className="wd-lessons list-group rounded-0">
             <li className="wd-lesson list-group-item p-3 ps-1">
+            <BsGripVertical className="me-2 fs-3" />
               Learning Objectives
-            <li className="wd-lesson list-group-item p-3 ps-1"> Lesson 1 </li>
-            <li className="wd-lesson list-group-item p-3 ps-1"> Lesson 2 </li>
+              <LessonControlButtons /> </li>
+            <li className="wd-lesson list-group-item p-3 ps-1"> 
+            <BsGripVertical className="me-2 fs-3" />
+            Lesson 1 <LessonControlButtons />
             </li>
+            <li className="wd-lesson list-group-item p-3 ps-1"> 
+            <BsGripVertical className="me-2 fs-3" />
+            Lesson 2  <LessonControlButtons /> </li>
           </ul>
         </li>
       </ul> 

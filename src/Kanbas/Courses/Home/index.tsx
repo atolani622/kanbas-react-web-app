ac084a087e79aaac2b9b920b6459cfed66da4ebd
AkshayTolani <107060419+AkshayTolani@users.noreply.github.com>
2024-10-07 14:23:19 -0400

    editing_home_courses

--- src/Kanbas/Courses/Home/index.tsx
+++ src/Kanbas/Courses/Home/index.tsx
@@ -2,26 +2,20 @@ import Modules from "../Modules";
 import CourseStatus from "./Status";
 export default function Home() {
   return (
-    <table id="wd-home">
-      <tr>
-        <td valign = "top">
-          <button>Collapse All</button>
-          <button>View Progress</button>
-          <select id="wd-select-publish">
-                <option selected value="Publish All">Publish All</option>
-                <option value="Publish One">Publish One</option>
-            </select>
-          <button>+Module</button>
-        </td>
-      </tr>
-      <tr>
-        <td valign="top">
-          <Modules />
-        </td>
-        <td valign="top">
-          <CourseStatus />
-        </td>
-      </tr>
-    </table>
+    <div className="d-flex" id="wd-home">
+      <div className="flex-fill">
+        <button>Collapse All</button>
+        <button>View Progress</button>
+        <select id="wd-select-publish">
+          <option selected value="Publish All">Publish All</option>
+          <option value="Publish One">Publish One</option>
+        </select>
+        <button>+Module</button>
+        <Modules />
+      </div>
+      <div className="d-none d-md-block">
+      <CourseStatus />
+    </div >
+    </div>
   );
 }

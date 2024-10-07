ec4a6ce3cef62f31dbe092ef70cae8a59025461b
AkshayTolani <107060419+AkshayTolani@users.noreply.github.com>
2024-10-07 15:44:25 -0400

    updated_module_controls

--- src/Kanbas/Courses/Home/index.tsx
+++ src/Kanbas/Courses/Home/index.tsx
@@ -4,13 +4,6 @@ export default function Home() {
   return (
     <div className="d-flex" id="wd-home">
       <div className="flex-fill">
-        <button>Collapse All</button>
-        <button>View Progress</button>
-        <select id="wd-select-publish">
-          <option selected value="Publish All">Publish All</option>
-          <option value="Publish One">Publish One</option>
-        </select>
-        <button>+Module</button>
         <Modules />
       </div>
       <div className="d-none d-md-block">

32b0a6ac7b9cb910a48e8732143e76589b1e8a71
AkshayTolani <107060419+AkshayTolani@users.noreply.github.com>
2024-10-08 11:44:56 -0400

    updated_account

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom";
 export default function AccountNavigation() {
   return (
-    <div id="wd-account-navigation">
-      <Link to={`/Kanbas/Account/Signin`}  > Signin  </Link> <br/>
-      <Link to={`/Kanbas/Account/Signup`}  > Signup  </Link> <br/>
-      <Link to={`/Kanbas/Account/Profile`} > Profile </Link> <br/>
+    <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
+      <Link id="wd-signin-link" to="/Kanbas/Account/Signin" className="list-group-item active border border-0"> 
+      Signin  </Link>
+      <Link id = "wd-signup-link" to="/Kanbas/Account/Signup" className = "list-group-item text-danger border border-0"> 
+      Sign up</Link>
+      <Link id = "wd-profile-link" to = "/Kanbas/Account/Profile" className = "list-group-item text-danger border border-0">
+        Profile
+      </Link>
     </div>
-);}
+  );
+}
--- src/Kanbas/Account/SignIn.tsx
+++ src/Kanbas/Account/SignIn.tsx
@@ -1,13 +1,19 @@
 import { Link } from "react-router-dom";
+
 export default function Signin() {
   return (
     <div id="wd-signin-screen">
       <h3>Sign in</h3>
-      <input className="wd-username" placeholder="username" /> <br />
-      <input className="wd-password" placeholder="password" type="password" /> <br />
-      <Link id="wd-signin-btn" to="/Kanbas/Dashboard"></Link>
-      <Link  id="wd-signin-btn"
-             to="/Kanbas/Account/Profile"> Sign in </Link> <br />
-      <Link  id="wd-signup-link" to="/Kanbas/Account/Signup">Sign up</Link>
+      <input id="wd-username"
+             placeholder="username"
+             className="form-control mb-2"/>
+      <input id="wd-password"
+             placeholder="password" type="password"
+             className="form-control mb-2"/>
+      <Link id="wd-signin-btn"
+            to="/Kanbas/Account/Profile"
+            className="btn btn-primary w-100">
+            Sign in </Link>
+      <Link id="wd-signup-link" to="/Kanbas/Account/Signup">Sign up</Link>
     </div>
 );}

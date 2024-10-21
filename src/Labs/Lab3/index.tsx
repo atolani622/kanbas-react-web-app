222611f137a7f110e0647718c43fda51f4536af4
AkshayTolani <107060419+AkshayTolani@users.noreply.github.com>
2024-10-21 17:58:37 -0400

    finished_lab

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -10,6 +10,7 @@ import FindIndex from "./FindIndex";
 import ForLoops from "./ForLoops";
 import IfElse from "./IfElse";
 import ImpliedReturn from "./ImpliedReturn";
+import JsonStringify from "./JsonStringify";
 import LegacyFunctions from "./LegacyFunctions";
 import MapFunction from "./MapFunction";
 import SimpleArrays from "./SimpleArrays";
@@ -17,9 +18,24 @@ import TemplateLiterals from "./TemplateLiterals";
 import TernaryOperator from "./TernaryOperator";
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
+import House from "./House"
+import TodoItem from "./todos/ToDoItem";
+import TodoList from "./todos/ToDoList";
+import Spreading from "./Spreading";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
+import Styles from "./Styles"
+import Add from "./Add"
+import Square from "./Square";
+import Highlight from "../Highlight";
+import AddPathParameters from "./AddPathParameters";
+import PathParameters from "./PathParameters";
 
 
 export default function Lab3() {
+  console.log('Hello World!');
   return (
     <div id="wd-lab3">
       <h3>Lab 3</h3>
@@ -42,6 +58,26 @@ export default function Lab3() {
       <FindFunction />
       <FindIndex />
       <FilterFunction />
+      <JsonStringify />
+      <House />
+      <TodoItem />
+      <TodoList />
+      <Spreading />
+      <Destructing />
+      <FunctionDestructing />
+      <DestructingImports />
+      <Classes />
+      <Styles />
+      <Add a={3} b={4} />
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <hr />
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+     </Highlight>
+     <AddPathParameters />
+     <PathParameters />
     </div>
   );
 }

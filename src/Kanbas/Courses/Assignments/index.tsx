25f64f5a0a7517644e5c31fb5f9a0728e17a8173
AkshayTolani <107060419+AkshayTolani@users.noreply.github.com>
2024-10-22 14:11:59 -0400

    fixed_assignments

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -3,8 +3,12 @@ import AssignmentControls from "./AssignmentControls";
 import AssignmentHeaderControls from "./AssignmentHeaderControls";
 import { RxPencil2 } from "react-icons/rx";
 import { BsGripVertical } from 'react-icons/bs';
+import * as db from "../../Database"
+import { useParams } from "react-router";
 
 export default function Assignments() {
+    const { cid } = useParams();
+    const assignments = db.assignments;
     return (
         <div>
             <AssignmentControls /> <br /> <br /> <br /> <br />
@@ -15,203 +19,36 @@ export default function Assignments() {
                         <AssignmentHeaderControls />
                     </div>
                     <ul className="wd-lessons list-group rounded-0">
-                        <li className="wd-lesson list-group-item p-3 ps-1">
-                            <div className="d-flex justify-content-between align-items-start">
-                                <div className="d-flex flex-column">
-                                    <div className="d-flex align-items-center">
-                                        <BsGripVertical className="me-2 fs-3" />
-                                        <RxPencil2 className="me-2 fs-3" />
-                                        <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">
-                                            A1 - ENV + HTML
-                                        </a>
-                                    </div>
-                                    <div className="assignment-details ms-5">
-                                        <div className="assignment-info">
-                                            <span className="text-red">Multiple Modules</span> | <b>Not available until</b> May 6 at 12:00am
+                        {assignments.filter(assignment => assignment.course === cid)
+                        .map((assignment) => (
+                            <li key={assignment._id} className="wd-lesson list-group-item p-3 ps-1">
+                                <div className="d-flex justify-content-between align-items-start">
+                                    <div className="d-flex flex-column">
+                                        <div className="d-flex align-items-center">
+                                            <BsGripVertical className="me-2 fs-3" />
+                                            <RxPencil2 className="me-2 fs-3" />
+                                            <a className="wd-assignment-link" href={`#/Kanbas/Courses/1234/Assignments/${assignment._id}`}>
+                                                {assignment.title}
+                                            </a>
                                         </div>
-                                        <div className="assignment-info">
-                                            <b>Due</b> May 13th at 11:59pm | 100pts
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="align-self-start mt-n2">
-                                    <AssignmentControlButtons />
-                                </div>
-                            </div>
-                        </li>
-                        <li className="wd-lesson list-group-item p-3 ps-1">
-                            <div className="d-flex justify-content-between align-items-start">
-                                <div className="d-flex flex-column">
-                                    <div className="d-flex align-items-center">
-                                        <BsGripVertical className="me-2 fs-3" />
-                                        <RxPencil2 className="me-2 fs-3" />
-                                        <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">
-                                            A2 - Assignment 2
-                                        </a>
-                                    </div>
-                                    <div className="assignment-details ms-5">
-                                        <div className="assignment-info">
-                                            <span className="text-red">Mulitple Modules</span> | <b>Not available until</b> May 6 at 12:00am |
-                                        </div>
-                                        <div className="assignment-info">
-                                            <b>Due</b> May 13th at 11:59pm | 100pts
+                                        <div className="assignment-details ms-5">
+                                            <div className="assignment-info">
+                                                <span className="text-red"></span> | <b></b>
+                                            </div>
+                                            <div className="assignment-info">
+                                                <b>Due</b> 10/5/2024 | 100pts
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div className="align-self-start mt-n2">
-                                    <AssignmentControlButtons />
-                                </div>
-                            </div>
-                        </li>
-                        <li className="wd-lesson list-group-item p-3 ps-1">
-                            <div className="d-flex justify-content-between align-items-start">
-                                <div className="d-flex flex-column">
-                                    <div className="d-flex align-items-center">
-                                        <BsGripVertical className="me-2 fs-3" />
-                                        <RxPencil2 className="me-2 fs-3" />
-                                        <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">
-                                            A3 - Assignment 3
-                                        </a>
-                                    </div>
-                                    <div className="assignment-details ms-5">
-                                        <div className="assignment-info">
-                                            <span className="text-red">Mulitple Modules</span> | <b>Not available until</b> May 6 at 12:00am |
-                                        </div>
-                                        <div className="assignment-info">
-                                            <b>Due</b> May 13th at 11:59pm | 100pts
-                                        </div>
+                                    <div className="align-self-start mt-n2">
+                                        <AssignmentControlButtons />
                                     </div>
                                 </div>
-                                <div className="align-self-start mt-n2">
-                                    <AssignmentControlButtons />
-                                </div>
-                            </div>
-                        </li>
-                        <li className="wd-lesson list-group-item p-3 ps-1">
-                            <div className="d-flex justify-content-between align-items-start">
-                                <div className="d-flex flex-column">
-                                    <div className="d-flex align-items-center">
-                                        <BsGripVertical className="me-2 fs-3" />
-                                        <RxPencil2 className="me-2 fs-3" />
-                                        <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">
-                                            A4 - Assignment 4
-                                        </a>
-                                    </div>
-                                    <div className="assignment-details ms-5">
-                                        <div className="assignment-info">
-                                            <span className="text-red">Mulitple Modules</span> | <b>Not available until</b> May 6 at 12:00am |
-                                        </div>
-                                        <div className="assignment-info">
-                                            <b>Due</b> May 13th at 11:59pm | 100pts
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="align-self-start mt-n2">
-                                    <AssignmentControlButtons />
-                                </div>
-                            </div>
-                        </li>
-                        <li className="wd-lesson list-group-item p-3 ps-1">
-                            <div className="d-flex justify-content-between align-items-start">
-                                <div className="d-flex flex-column">
-                                    <div className="d-flex align-items-center">
-                                        <BsGripVertical className="me-2 fs-3" />
-                                        <RxPencil2 className="me-2 fs-3" />
-                                        <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">
-                                            A5 - Assignment 5
-                                        </a>
-                                    </div>
-                                    <div className="assignment-details ms-5">
-                                        <div className="assignment-info">
-                                            <span className="text-red">Mulitple Modules</span> | <b>Not available until</b> May 6 at 12:00am |
-                                        </div>
-                                        <div className="assignment-info">
-                                            <b>Due</b> May 13th at 11:59pm | 100pts
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="align-self-start mt-n2">
-                                    <AssignmentControlButtons />
-                                </div>
-                            </div>
-                        </li>
-                        <li className="wd-lesson list-group-item p-3 ps-1">
-                            <div className="d-flex justify-content-between align-items-start">
-                                <div className="d-flex flex-column">
-                                    <div className="d-flex align-items-center">
-                                        <BsGripVertical className="me-2 fs-3" />
-                                        <RxPencil2 className="me-2 fs-3" />
-                                        <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">
-                                            A6 - Assignment 6
-                                        </a>
-                                    </div>
-                                    <div className="assignment-details ms-5">
-                                        <div className="assignment-info">
-                                            <span className="text-red">Mulitple Modules</span> | <b>Not available until</b> May 6 at 12:00am |
-                                        </div>
-                                        <div className="assignment-info">
-                                            <b>Due</b> May 13th at 11:59pm | 100pts
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="align-self-start mt-n2">
-                                    <AssignmentControlButtons />
-                                </div>
-                            </div>
-                        </li>
-                        <li className="wd-lesson list-group-item p-3 ps-1">
-                            <div className="d-flex justify-content-between align-items-start">
-                                <div className="d-flex flex-column">
-                                    <div className="d-flex align-items-center">
-                                        <BsGripVertical className="me-2 fs-3" />
-                                        <RxPencil2 className="me-2 fs-3" />
-                                        <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">
-                                            A7 - Assignment 7
-                                        </a>
-                                    </div>
-                                    <div className="assignment-details ms-5">
-                                        <div className="assignment-info">
-                                            <span className="text-red">Mulitple Modules</span> | <b>Not available until</b> May 6 at 12:00am |
-                                        </div>
-                                        <div className="assignment-info">
-                                            <b>Due</b> May 13th at 11:59pm | 100pts
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="align-self-start mt-n2">
-                                    <AssignmentControlButtons />
-                                </div>
-                            </div>
-                        </li>
-                        <li className="wd-lesson list-group-item p-3 ps-1">
-                            <div className="d-flex justify-content-between align-items-start">
-                                <div className="d-flex flex-column">
-                                    <div className="d-flex align-items-center">
-                                        <BsGripVertical className="me-2 fs-3" />
-                                        <RxPencil2 className="me-2 fs-3" />
-                                        <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">
-                                            A8 - Assignment 8
-                                        </a>
-                                    </div>
-                                    <div className="assignment-details ms-5">
-                                        <div className="assignment-info">
-                                            <span className="text-red">Mulitple Modules</span> | <b>Not available until</b> May 6 at 12:00am |
-                                        </div>
-                                        <div className="assignment-info">
-                                            <b>Due</b> May 13th at 11:59pm | 100pts
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="align-self-start mt-n2">
-                                    <AssignmentControlButtons />
-                                </div>
-                            </div>
-                        </li>
+                            </li>
+                        ))}
                     </ul>
                 </li>
             </ul>
         </div>
-
-
     );
 }

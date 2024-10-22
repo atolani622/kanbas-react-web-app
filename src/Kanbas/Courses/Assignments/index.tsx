ded3bc7361348f4851c8410cdc5e31d7ce1f6237
AkshayTolani <107060419+AkshayTolani@users.noreply.github.com>
2024-10-22 15:57:57 -0400

    updated_assignments

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -19,33 +19,32 @@ export default function Assignments() {
                         <AssignmentHeaderControls />
                     </div>
                     <ul className="wd-lessons list-group rounded-0">
-                        {assignments.filter(assignment => assignment.course === cid)
-                        .map((assignment) => (
-                            <li key={assignment._id} className="wd-lesson list-group-item p-3 ps-1">
-                                <div className="d-flex justify-content-between align-items-start">
-                                    <div className="d-flex flex-column">
-                                        <div className="d-flex align-items-center">
-                                            <BsGripVertical className="me-2 fs-3" />
-                                            <RxPencil2 className="me-2 fs-3" />
-                                            <a className="wd-assignment-link" href={`#/Kanbas/Courses/1234/Assignments/${assignment._id}`}>
-                                                {assignment.title}
-                                            </a>
+                        {assignments
+                            .filter(assignment => assignment.course === cid)
+                            .map((assignment) => (
+                                <li key={assignment._id} className="wd-lesson list-group-item p-3 ps-1">
+                                    <div className="d-flex justify-content-between align-items-start">
+                                        <div className="d-flex flex-column">
+                                            <div className="d-flex align-items-center">
+                                                <BsGripVertical className="me-2 fs-3" />
+                                                <RxPencil2 className="me-2 fs-3" />
+                                                <a className="wd-assignment-link" href={`#/Kanbas/Courses/${assignment.course}/Assignments/${assignment._id}`}>
+                                                    {assignment.title}
+                                                </a>
+                                            </div>
+                                            <div className="assignment-details ms-5">
+                                                <div className="assignment-info">
+                                                    <span className="text-red">{assignment.description}</span> | <b> Not availale until </b>{assignment.available_date}
+                                                      | <b>Due</b> {assignment.due_date} | {assignment.points}pts
+                                                </div>
+                                            </div>
                                         </div>
-                                        <div className="assignment-details ms-5">
-                                            <div className="assignment-info">
-                                                <span className="text-red"></span> | <b></b>
-                                            </div>
-                                            <div className="assignment-info">
-                                                <b>Due</b> 10/5/2024 | 100pts
-                                            </div>
+                                        <div className="align-self-start mt-n2">
+                                            <AssignmentControlButtons />
                                         </div>
                                     </div>
-                                    <div className="align-self-start mt-n2">
-                                        <AssignmentControlButtons />
-                                    </div>
-                                </div>
-                            </li>
-                        ))}
+                                </li>
+                            ))}
                     </ul>
                 </li>
             </ul>

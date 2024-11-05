be5a561f442ba96efdc9a66f8691cb3d9c5e3864
AkshayTolani <107060419+AkshayTolani@users.noreply.github.com>
2024-11-05 11:44:40 -0500

    lab4

---
+++ src/Labs/Lab4/index.tsx
@@ -0,0 +1,10 @@
+import ClickEvent from "./ClickEvent";
+
+export default function Lab4() {
+    return (
+        <div id="wd-lab4">
+            <h4>Lab 4</h4>
+            <ClickEvent />
+        </div>
+    );
+}

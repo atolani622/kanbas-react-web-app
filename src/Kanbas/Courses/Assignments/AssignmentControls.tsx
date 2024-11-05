a2f1bcde62f734b2219cca4e9e0c084802244c00
AkshayTolani <107060419+AkshayTolani@users.noreply.github.com>
2024-11-05 15:27:30 -0500

    added_+assignment

--- src/Kanbas/Courses/Assignments/AssignmentControls.tsx
+++ src/Kanbas/Courses/Assignments/AssignmentControls.tsx
@@ -1,15 +1,18 @@
 import { FaPlus } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 
 export default function AssignmentControls() {
     return (
         <div id="wd-modules-controls" className="text-nowrap">
             <button id="wd-add-module-btn" className="btn btn-lg btn-danger me-1 float-end">
-                <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
-                Assignment</button>
-                <button id="wd-add-module-btn" className="btn btn-lg btn-danger me-1 float-end">
+                <Link to={"../Assignments/Editor"} className="btn btn-danger btn-sm" role="button">
+                    <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
+                    Assignment
+                </Link></button>
+            <button id="wd-add-module-btn" className="btn btn-lg btn-danger me-1 float-end">
                 <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
                 Group</button>
         </div>
-);
+    );
 }

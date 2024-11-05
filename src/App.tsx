809aa73a27dc706e401c0960f04df7ef81184468
AkshayTolani <107060419+AkshayTolani@users.noreply.github.com>
2024-11-05 13:44:30 -0500

    added_protected_route

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import Kanbas from "./Kanbas"
 import Labs from "./Labs"
 import logo from './logo.svg';
 import './App.css';
+import store from "./Kanbas/store"
+import { Provider } from 'react-redux';
+
 function App() {
   return (
     <HashRouter>
+      <Provider store={store}>
       <div>
         <Routes>
           <Route path="/" element={<Navigate to="Kanbas" />} />
@@ -14,6 +18,7 @@ function App() {
           <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
       </div>
+      </Provider>
     </HashRouter>
   );
 }

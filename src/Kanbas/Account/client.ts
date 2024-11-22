ab986de2a9204aaa5f128514a53a9ab39892c04c
AkshayTolani <107060419+AkshayTolani@users.noreply.github.com>
2024-11-22 16:15:46 -0500

    up_to_edit_course

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -26,5 +26,11 @@ export const updateUser = async (user: any) => {
 export const findMyCourses = async () => {
     const { data } = await axiosWithCredentials.get(`${USERS_API}/current/courses`);
     return data;
-  };
-  
+};
+
+export const createCourse = async (course: any) => {
+    const { data } = await axiosWithCredentials.post(`${USERS_API}/current/courses`, course);
+    return data;
+};
+
+
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -9,6 +9,7 @@ import ProtectedRoute from "./Account/ProtectedRoute";
 import Session from "./Account/Session";
 import * as userClient from "./Account/client"
 import { useSelector } from "react-redux";
+import * as courseClient from "./Courses/client";
 
 export default function Kanbas() {
     const [courses, setCourses] = useState<any[]>([]);
@@ -29,23 +30,24 @@ export default function Kanbas() {
         _id: "1234", name: "New Course", number: "New Number",
         startDate: "2023-09-10", endDate: "2023-12-15", description: "New Description",
     });
-    const addNewCourse = () => {
-        setCourses([...courses, { ...course, _id: new Date().getTime().toString() }]);
-    };
-    const deleteCourse = (courseId: any) => {
+    const addNewCourse = async () => {
+        const newCourse = await userClient.createCourse(course);
+        setCourses([ ...courses, newCourse ]);
+      };
+    
+      const deleteCourse = async (courseId: string) => {
+        const status = await courseClient.deleteCourse(courseId);
         setCourses(courses.filter((course) => course._id !== courseId));
-    };
-    const updateCourse = () => {
-        setCourses(
-            courses.map((c) => {
-                if (c._id === course._id) {
-                    return course;
-                } else {
-                    return c;
-                }
-            })
-        );
-    };
+      };
+    
+      const updateCourse = async () => {
+        await courseClient.updateCourse(course);
+        setCourses(courses.map((c) => {
+            if (c._id === course._id) { return course; }
+            else { return c; }
+        })
+      );};
+    
 
     return (
         <Session>

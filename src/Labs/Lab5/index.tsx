616361e43c39394faac4b7ce1affc5ac0e79f428
AkshayTolani <107060419+AkshayTolani@users.noreply.github.com>
2024-11-22 12:27:36 -0500

    updated_lab5

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -2,6 +2,7 @@ import EnvironmentVariables from "./EnviormentVariables";
 import HttpClient from "./HTTPClient";
 import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
+import WorkingWithObjectsAsynchronously from "./WoringWithObjectsAsynchronously";
 import WorkingWithArrays from "./WorkingWithArrays";
 import WorkingWithObjects from "./WorkingWithObjects";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
@@ -23,6 +24,7 @@ export default function Lab5() {
         <WorkingWithObjects />
         <WorkingWithArrays />
         <HttpClient />
+        <WorkingWithObjectsAsynchronously />
       </div>
     );
   }

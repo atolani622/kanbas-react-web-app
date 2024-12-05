22bd927232d0a0e1492e02c9f1c4febe51b98a90
Akshay Tolani <akshayshaam@gmail.com>
2024-12-05 14:11:16 -0500

    added_users_table

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -4,6 +4,7 @@ import Signup from "./SignUp";
 import Profile from "./Profile";
 import AccountNavigation from "./Navigation";
 import { useSelector } from "react-redux";
+import Users from "./Users";
 
 
 export default function Account() {
@@ -21,6 +22,7 @@ export default function Account() {
                         <Route path="/Signin" element={<Signin />} />
                         <Route path="/Profile" element={<Profile />} />
                         <Route path="/Signup" element={<Signup />} />
+                        <Route path="/Users" element={<Users />} />
                     </Routes>
                 </td>
             </tr>
--- src/Kanbas/Courses/People/Table.tsx
+++ src/Kanbas/Courses/People/Table.tsx
@@ -1,22 +1,19 @@
 import { FaUserCircle } from "react-icons/fa";
 import { useParams } from "react-router-dom";
 import * as db from "../../Database";
+import React, { useState, useEffect } from "react";
 
-export default function PeopleTable() {
-  const { cid } = useParams();
-  const { users, enrollments } = db;
+
+export default function PeopleTable({ users = [] }: { users?: any[] }) {
+  // const { cid } = useParams();
+  // const { users, enrollments } = db;
   return (
     <div id="wd-people-table">
       <table className="table table-striped">
-        <thead>
-          <tr><th>Name</th><th>Login ID</th><th>Section</th><th>Role</th><th>Last Activity</th><th>Total Activity</th></tr>
-        </thead>
+        ...
         <tbody>
-  {users
-    .filter((usr) =>
-      enrollments.some((enrollment) => enrollment.user === usr._id && enrollment.course === cid)
-    )
-    .map((user: any) => (
+          {users
+            .map((user: any) => (
       <tr key={user._id}>
         <td className="wd-full-name text-nowrap">
           <FaUserCircle className="me-2 fs-1 text-secondary" />

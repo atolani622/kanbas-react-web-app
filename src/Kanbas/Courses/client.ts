748dfc660a7fd94871071a32600e340a24b66529
Akshay Tolani <akshayshaam@gmail.com>
2024-12-07 20:51:15 -0500

    updated_course

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+const axiosWithCredentials = axios.create({ withCredentials: true });
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
 
-export const fetchAllCourses = async () => {
-    const { data } = await axios.get(COURSES_API);
+export const fetchAllCourses = async () => {    
+    const { data } = await axiosWithCredentials.get(COURSES_API);
     return data;
 };
 

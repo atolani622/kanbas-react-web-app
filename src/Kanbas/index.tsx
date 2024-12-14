9a0716fc5bc1ee2b3581fe8a7f580d9b0347fcd9
Akshay Tolani <akshayshaam@gmail.com>
2024-12-14 10:45:02 -0500

    a6

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -7,24 +7,50 @@ import "./styles.css"
 import { useEffect, useState } from "react";
 import ProtectedRoute from "./Account/ProtectedRoute";
 import Session from "./Account/Session";
-import * as userClient from "./Account/client"
 import { useSelector } from "react-redux";
 import * as courseClient from "./Courses/client";
+import * as userClient from "./Account/client";
 
 export default function Kanbas() {
     const [courses, setCourses] = useState<any[]>([]);
     const { currentUser } = useSelector((state: any) => state.accountReducer);
+    const [enrolling, setEnrolling] = useState<boolean>(false);
+
+    const findCoursesForUser = async () => {
+      try {
+        const courses = await userClient.findCoursesForUser(currentUser._id);
+        setCourse(courses);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+   
     const fetchCourses = async () => {
       try {
-        const courses = await courseClient.fetchAllCourses();
+        const allCourses = await courseClient.fetchAllCourses();
+        const enrolledCourses =  await userClient.findCoursesForUser(
+          currentUser._id
+        );
+        const courses = allCourses.map((course: any) => {
+          if (enrolledCourses.find((c: any) => c._id === course._id)) {
+            return { ...course, enrolled: true };
+          } else {
+            return course;
+          }
+        });
         setCourses(courses);
       } catch (error) {
         console.error(error);
       }
     };
+   
     useEffect(() => {
-      fetchCourses();
-    }, [currentUser]);
+      if (enrolling) {
+        fetchCourses();
+      } else {
+        findCoursesForUser();
+      }
+    }, [currentUser, enrolling]);
   
     const [course, setCourse] = useState<any>({
         _id: "1234", name: "New Course", number: "New Number",
@@ -49,6 +75,24 @@ export default function Kanbas() {
             else { return c; }
         })
       );};
+
+      const updateEnrollment = async (courseId: string, enrolled: boolean) => {
+        if (enrolled) {
+          await userClient.enrollIntoCourse(currentUser._id, courseId);
+        } else {
+          await userClient.unenrollFromCourse(currentUser._id, courseId);
+        }
+        setCourses(
+          courses.map((course) => {
+            if (course._id === courseId) {
+              return { ...course, enrolled: enrolled };
+            } else {
+              return course;
+            }
+          })
+        );
+      };
+     
     
 
     return (
@@ -65,7 +109,10 @@ export default function Kanbas() {
                             setCourse={setCourse}
                             addNewCourse={addNewCourse}
                             deleteCourse={deleteCourse}
-                            updateCourse={updateCourse} /></ProtectedRoute>} />
+                            updateCourse={updateCourse}
+                            enrolling={enrolling} 
+                            setEnrolling={setEnrolling}
+                            updateEnrollment={updateEnrollment} /></ProtectedRoute>} />
                         <Route path="/Courses/:cid/*" element={<ProtectedRoute><Courses courses={courses} /></ProtectedRoute>} />
                         <Route path="/Calendar" element={<h1>Calendar</h1>} />
                         <Route path="/Inbox" element={<h1>Inbox</h1>} />
